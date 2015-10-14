57ac44c3a5ef19f18128692dc9f5c71fda3dfb70
Sivarat Malapet <sivarat.malapet@clbs.co.th>
2015-10-14 13:56:54 +0700

    Add copyright

--- mongoNode1.js
+++ mongoNode1.js
@@ -1,3 +1,7 @@
+/*
+Simple RESTFul WS implemented by Node.JS and MongoDB
+Copyright 2015 Sivarat Malapet. All rights reserved.
+*/
 var express = require('express');
 var bodyParser = require('body-parser')
 
